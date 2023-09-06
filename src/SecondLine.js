a63a02b66bb422fb62340c7155db8da6a3485944
Alma Duffey <1984ahern@gmail.com>
2023-09-06 14:40:41 -0500

    starting second page

--- src/SecondLine.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./SecondLine.css"
-const SecondLine = () => {
-    return (
-        <div className = "SecondLine">Converter for Multiple Currencies</div>
-    )
-};
-
-export default SecondLine;
